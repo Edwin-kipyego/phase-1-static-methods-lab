83f354ee81daa42c0e2bdcb99d1e73dda61418f4
Edwin kipyego <koriredwin95@gmail.com>
2025-04-03 16:08:48 +0300

    static methods

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
-  //add static methods here
+  // Capitalizes the first letter of a string
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  // Removes non-alphanumeric characters except for dash, single quote, and space
+  static sanitize(str) {
+    return str.replace(/[^a-zA-Z0-9-' ]/g, '');
+  }
+
+  // Capitalizes all words except small words, but always capitalizes the first word
+  static titleize(sentence) {
+    const smallWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    return sentence
+      .split(' ')
+      .map((word, index) => {
+        if (index === 0 || !smallWords.includes(word.toLowerCase())) {
+          return this.capitalize(word);
+        }
+        return word.toLowerCase();
+      })
+      .join(' ');
+  }
 }
+
+module.exports = Formatter;
